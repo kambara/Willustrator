e97d6266a9eeb129d1fb31cb450f251db37e0aa3
kambara <kambara@94f79cfe-0f1f-0410-b843-a4e0638dffbe>
2006-10-25 13:57:44 +0000

    git-svn-id: svn+ssh://kampara.sakura.ne.jp/home/kampara/svn/repos/willustrator/trunk/Willustrator@15 94f79cfe-0f1f-0410-b843-a4e0638dffbe

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -41,21 +41,21 @@ class JsonController < ApplicationController
   end
 
   def new_image
+    json = ''
     if (!@session[:user_name])
-      render :text => {:error => 'need to login'}.to_json
-      return
+      json = {:error => 'need to login'}.to_json
+    else
+      site_url = "http://#{@request.env['HTTP_HOST']}"
+      image = Image.new_item @session[:user_name]
+      json = {
+        :owner => @session[:user_name],
+        :link  => "#{site_url}/image/#{image.md5id.to_s}",
+        :edit  => "#{site_url}/image/new_item/#{image.md5id.to_s}",
+        :png   => "#{site_url}/data/#{image.md5id.to_s}.png",
+        :small => "#{site_url}/data/#{image.md5id.to_s}_small.png",
+        :svg   => "#{site_url}/data/#{image.md5id.to_s}.svg",
+      }.to_json
     end
-    site_url = "http://#{@request.env['HTTP_HOST']}"
-    image = Image.new_item @session[:user_name]
-    json = {
-      :owner => @session[:user_name],
-      :link  => "#{site_url}/image/#{image.md5id.to_s}",
-      :edit  => "#{site_url}/image/new_item/#{image.md5id.to_s}",
-      :png   => "#{site_url}/data/#{image.md5id.to_s}.png",
-      :small => "#{site_url}/data/#{image.md5id.to_s}_small.png",
-      :svg   => "#{site_url}/data/#{image.md5id.to_s}.svg",
-    }.to_json
-
     if (@params['jsonp'])
       render :text => "#{@params['jsonp']}(#{json});"
     else
